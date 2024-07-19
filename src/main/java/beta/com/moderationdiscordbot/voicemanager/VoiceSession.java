0806de6cc26bab93b10be7604e732816e4234520
Tuna Rasim OCAK <tunarasimocak@gmail.com>
2024-07-19 15:57:25 +0300

    Update

--- src/main/java/beta/com/moderationdiscordbot/voicemanager/VoiceSession.java
+++ src/main/java/beta/com/moderationdiscordbot/voicemanager/VoiceSession.java
@@ -3,12 +3,15 @@ package beta.com.moderationdiscordbot.voicemanager;
 import beta.com.moderationdiscordbot.autopunish.antiswear.AntiSwear;
 import beta.com.moderationdiscordbot.databasemanager.ServerSettings.ServerSettings;
 import beta.com.moderationdiscordbot.langmanager.LanguageManager;
+import beta.com.moderationdiscordbot.utils.EmbedBuilderManager;
 import beta.com.moderationdiscordbot.voicemanager.auidomanager.AudioReceiver;
 import beta.com.moderationdiscordbot.voicemanager.auidomanager.SpeechToTextAPI;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.managers.AudioManager;
 import net.dv8tion.jda.api.entities.Member;
 
 import javax.sound.sampled.UnsupportedAudioFileException;
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
@@ -20,6 +23,7 @@ public class VoiceSession {
     private final ServerSettings serverSettings;
     private final LanguageManager languageManager;
     private AudioReceiver audioReceiver;
+    private final EmbedBuilderManager embedBuilderManager;
 
     public VoiceSession(AudioManager audioManager, String userDir, AntiSwear antiSwear, Member member, ServerSettings serverSettings, LanguageManager languageManager) {
         this.audioManager = audioManager;
@@ -28,6 +32,7 @@ public class VoiceSession {
         this.member = member;
         this.serverSettings = serverSettings;
         this.languageManager = languageManager;
+        this.embedBuilderManager = new EmbedBuilderManager(languageManager);
     }
 
     public void startRecording() {
@@ -49,5 +54,18 @@ public class VoiceSession {
             }
             new File(userDir + "/output.wav").delete(); // delete the audio file after analysis
         }
+    }
+
+    private void sendWarningDM() {
+        String language = serverSettings.getLanguage(member.getGuild().getId());
+        EmbedBuilder embed = embedBuilderManager.createEmbedWithColor(
+                "events.voice.warning.title",
+                "events.voice.warning.description",
+                language,
+                Color.RED
+        );
+        member.getUser().openPrivateChannel().queue(channel ->
+                channel.sendMessageEmbeds(embed.build()).queue()
+        );
     }
 }
